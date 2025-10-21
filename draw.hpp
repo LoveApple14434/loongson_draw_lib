cf7542874f0e61e65bc6a521fe5a77a58cc829f6
Harry <szy1985671808@163.com>
2025-10-21 14:44:07 +0800

    fix: offer api to get resolution

--- draw.hpp
+++ draw.hpp
@@ -8,6 +8,11 @@ typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 
+int fb_init();
+int fb_close();
+u16 get_xres();
+u16 get_yres();
+
 void draw_point(u16 x, u16 y, u32 color);
 void draw_line(u16 x1, u16 y1, u16 x2, u16 y2, u32 color);
 void draw_block(u16 x1, u16 y1, u16 x2, u16 y2, u32 color);
--- draw_basic.cpp
+++ draw_basic.cpp
@@ -8,6 +8,7 @@
 static int fb_fd;
 static u32 * fb_ptr;
 static int width, height;
+static bool initialized= false;
 
 using vinfo= fb_var_screeninfo;
 using finfo= fb_fix_screeninfo;
@@ -19,10 +20,14 @@ int fb_init(){
     ioctl(fb_fd, FBIOGET_VSCREENINFO, &vars);
     width=vars.xres_virtual; height=vars.yres_virtual;
     fb_ptr = (uint32_t *)mmap(nullptr, vars.yres_virtual * vars.xres_virtual * 4, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, 0);
+    initialized= true;
     return 0;
 }
 
 int fb_close(){
+    if (!initialized) {
+        return -1;
+    }
     munmap(fb_ptr, height * width * 4);
     close(fb_fd);
     return 0;
@@ -32,4 +37,22 @@ void draw_point(u16 x, u16 y, u32 color){
     if (x>=width || x<0 || y>=height || y<0) return;
     fb_ptr[y*width + x]=color;
     return;
+}
+
+u16 get_xres(){
+    if (initialized) {
+        return width;
+    }
+    else {
+        return 0;
+    }
+}
+
+u16 get_yres(){
+    if (initialized) {
+        return height;
+    }
+    else {
+        return 0;
+    }
 }
